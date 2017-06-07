00096db5157437b8db1dcf58422be7a6b0a7389d
akuchyn <forrana@gmail.com>
2017-06-07 19:54:30 +0300

    Add DB * add connectors, resolvers and schemas for needed dbs * add loadsh, sqlite (for mocks) and mongoose * add generation of mocks into db

---
+++ server/data/resolvers.js
@@ -0,0 +1,33 @@
+import { Channel } from './connectors';
+
+const resolvers = {
+  Query: {
+    author(root, args){
+      return { id: 1, firstName: 'Hello', lastName: 'World' };
+    },
+    channels(_, args) {
+        return Channel.findAll({ where: args });
+    },
+    // channels(root, args) {
+    //     return [
+    //         { id: 1, name: 'Hello' },
+    //         { id: 2, name: 'World' },
+    //     ]
+    // }
+  },
+  Author: {
+    posts(author){
+      return [
+        { id: 1, title: 'A post', text: 'Some text', views: 2},
+        { id: 2, title: 'Another post', text: 'Some other text', views: 200}
+      ];
+    },
+  },
+  Post: {
+    author(post){
+      return { id: 1, firstName: 'Hello', lastName: 'World' };
+    },
+  },
+};
+
+export default resolvers;
